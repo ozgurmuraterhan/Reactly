42d03030eb48ed341ce598e153ec089dea175936
ozgurmuraterhan <33369670+ozgurmuraterhan@users.noreply.github.com>
2020-07-04 15:54:18 +0300

    auth. editing register only admin

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -16,41 +16,53 @@ const signToken = (userID) => {
     );
 };
 
-router.post('/register', (req, res) => {
-    const { username, password, role } = req.body;
-    User.findOne({ username }, (err, user) => {
-        if (err)
-            res.status(500).json({
-                message: { msgBody: 'Error has occured', msgError: true },
-            });
-        if (user)
-            res.status(400).json({
-                message: {
-                    msgBody: 'Username is already taken',
-                    msgError: true,
-                },
-            });
-        else {
-            const newUser = new User({ username, password, role });
-            newUser.save((err) => {
+router.post('/register',
+    passport.authenticate('jwt', { session: false }), (req, res) => {
+        const { username, password, role } = req.body;
+
+        if (req.user.role === 'admin') {
+            User.findOne({ username }, (err, user) => {
                 if (err)
                     res.status(500).json({
+                        message: { msgBody: 'Error has occured', msgError: true },
+                    });
+                if (user)
+                    res.status(400).json({
                         message: {
-                            msgBody: 'Error has occured',
+                            msgBody: 'Username is already taken',
                             msgError: true,
                         },
                     });
-                else
-                    res.status(201).json({
-                        message: {
-                            msgBody: 'Account successfully created',
-                            msgError: false,
-                        },
+                else {
+                    const newUser = new User({ username, password, role });
+                    newUser.save((err) => {
+                        if (err)
+                            res.status(500).json({
+                                message: {
+                                    msgBody: 'Error has occured',
+                                    msgError: true,
+                                },
+                            });
+                        else
+                            res.status(201).json({
+                                message: {
+                                    msgBody: 'Account successfully created',
+                                    msgError: false,
+                                },
+                            });
                     });
+                }
             });
+        } else {
+            res.status(403).json({
+                message: {
+                    msgBody: "You're not an admin,go away",
+                    msgError: true,
+                },
+            });
+
         }
     });
-});
 
 router.post(
     '/login',
